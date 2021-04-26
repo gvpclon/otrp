e5d78f1345343cf3582d39fb278ebfe68c3d17ac
gvidaurre <gvpclon@gmail.com>
2021-04-26 11:54:07 -0400

    se agrego ciclos

---
+++ src/ciclos.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ciclos {
+    public static void main(String[] args) {
+        System.out.println("prueba");
+        Scanner entrada = new Scanner(System.in);
+
+    }
+}
